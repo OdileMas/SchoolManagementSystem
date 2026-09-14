301d1f6f683769b2306960c196d2b0ef4522666e
MASENGESHO Odile <masengeshoodile@gmail.com>
2026-09-14 16:22:42 +0200

    Add JWT authentication: custom login response, token refresh, role-based permission fix

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import RegisterView,  AdminCreateUserView
+from rest_framework_simplejwt.views import  TokenRefreshView
+from .views import RegisterView, AdminCreateUserView, CustomTokenObtainPairView
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("admin/create-user/", AdminCreateUserView.as_view(), name="admin-create-user"),
+    path("login/", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("login/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
